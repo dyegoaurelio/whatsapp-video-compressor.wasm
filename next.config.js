0fbe5fedca1b07180f4279011b8f430c8a6275bc
dyego <dyegoaurelio@gmail.com>
2022-10-15 11:57:22 -0300

    add extra Cache Control

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
         headers: [
           {
             key: "Cache-Control",
-            value: "public, max-age=31536000",
+            value: "public, max-age=31536000, immutable",
           },
         ],
       },
