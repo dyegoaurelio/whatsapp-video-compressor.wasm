63c2c2c7e5ba3dd6b3b586db00b8637fc1030103
dyego <dyegoaurelio@gmail.com>
2022-10-15 12:23:11 -0300

    remove typo

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === 'development',
 });
 
-module.exports = withPWA({
-  // next.js config
-});
-
 const nextConfig = {
   generateEtags: false,
   // reactStrictMode: true,
